47adf2db81ef0e85716f1b4a378e2f7ac37124a6
mirzalf01 <irzalfahriza0@gmail.com>
2021-04-13 09:20:46 +0700

    Style: Login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -47,3 +47,79 @@
         <a href="{{ route('register') }}">Klik disini untuk mendaftar</a>
     </x-jet-authentication-card>
 </x-guest-layout>
+ --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Keboen Coffee | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition login-page" style="background: url('{{ asset('logo/background.jpg') }}') center">
+<div class="login-box">
+<div class="card">
+    <div class="card-body login-card-body">
+        <div class="login-logo">
+            <img src="{{ asset('logo/logo1.png') }}" style="width: 50px; height: 50px" alt=""><b>Keboen</b>Coffee
+        </div>
+        <!-- /.login-logo -->
+        <p class="login-box-msg">Sign in to start your session</p>
+        @if ($errors->any())
+            <div class="text-danger">
+                <div class="font-medium text-red-600 text-center">{{ __('Email atau password salah') }}</div>
+            </div><br>
+        @endif
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+                <input type="email" name="email" required value="{{ old('email') }}" class="form-control" placeholder="Email">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="input-group mb-3">
+                <input type="password" required name="password" class="form-control" placeholder="Password">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="input-group mb-3">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+        </form>
+        <p class="mb-0">
+            <a href="{{ route('register') }}" class="text-center">Klik disini untuk mendaftar.</a>
+        </p>
+    </div>
+    <!-- /.login-card-body -->
+</div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('AdminLTE/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('AdminLTE/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
